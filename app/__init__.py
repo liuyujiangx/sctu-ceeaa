5927287728d5853b3314ba716fad44628166ad96
liuyujiangx <1914571065@qq.com>
2020-09-04 17:15:31 +0800

    更改

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,26 @@ from flask import Flask, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 from flask_cors import *
+
 app = Flask(__name__)
-CORS(app, supports_credentials=True,resources=r'/*')
+CORS(app, supports_credentials=True, resources=r'/*')
 import os
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://myroot:1914571065@47.95.235.93:3306/sctu-ceeaa'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 app.config["SECRET_KEY"] = '235c749859ec44c2bd6064ec6da7b927'
+app.config['EXPIRES_IN'] = 9999
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
+app.config['REDIS_HOST'] = '47.95.235.93'
+app.config['REDIS_PORT'] = 3306
+app.config['REDIS_DB'] = 'sctu-ceeaa'
+app.config['REDIS_PWD'] = '1914571065'
 app.debug = True
 db = SQLAlchemy(app)
 
-
-from app.routes import home as home_blueprint
+from app.home import home as home_blueprint
+from app.user import user as user_blueprint
 
 app.register_blueprint(home_blueprint)
+app.register_blueprint(user_blueprint, url_prefix="/user/")
